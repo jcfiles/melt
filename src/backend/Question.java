d03e5314def19b23851a8982299b9b9cca712c9d
Bruce Steedman <bruce.steedman@gmail.com>
2013-10-09 16:20:45 +0100

    Changed awarding of question marks such that mark is awarded only if all correct answers and no wrong answers are selected.

--- src/backend/Question.java
+++ src/backend/Question.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
- * Representation of a question with one or more correct answers
+ * Representation of a question with one or more answers.
  * 
  * @author Bruce Steedman
  * @version 2013.09.27
@@ -17,7 +17,7 @@ public class Question implements java.io.Serializable
     protected ArrayList<Answer> answers;
     
     /**
-     * Constructor for objects of class question
+     * Constructor for questions.
      */
     public Question(String questionText)
     {
@@ -26,7 +26,17 @@ public class Question implements java.io.Serializable
     }
     
     /**
-     * Constructor questions, with a single answer provided,
+     * Constructor for questions with number of marks provided.
+     */
+    public Question(String questionText, int mark)
+    {
+        this.questionText = questionText;
+        answers = new ArrayList<Answer>();
+        possibleMarks = mark;
+    }
+    
+    /**
+     * Constructor questions, with a single answer provided.
      */
     public Question(String questionText, Answer a)
     {
@@ -36,7 +46,7 @@ public class Question implements java.io.Serializable
     }
     
     /**
-     * Constructor questions, with a multiple answers provided,
+     * Constructor questions, with a multiple answers provided.
      */
     public Question(String questionText, ArrayList<Answer> answers)
     {
@@ -45,7 +55,7 @@ public class Question implements java.io.Serializable
     }
     
     /**
-     * Constructor questions, with a single answer and number of marks provided,
+     * Constructor questions, with a single answer and number of marks provided.
      */
     public Question(String questionText, Answer a, int mark)
     {
@@ -56,7 +66,7 @@ public class Question implements java.io.Serializable
     }
     
     /**
-     * Constructor questions, with multiple answers and number of marks provided,
+     * Constructor questions, with multiple answers and number of marks provided.
      */
     public Question(String questionText, ArrayList<Answer> answers, int mark)
     {
@@ -65,16 +75,13 @@ public class Question implements java.io.Serializable
         possibleMarks = mark;
     }
     
-    /**
-     * Adds an answer to the question
-     */
     public void addAnswer(String answerText, boolean isRight)
     {
         answers.add(new Answer(answerText, isRight));
     }
     
     /**
-     * Sets an answer to the question by index
+     * Sets an answer to the question by index, e.g. 0 is the first answer. 
      */
     public void setAnswer(String answerText, boolean isRight, int index)
     {
@@ -82,7 +89,7 @@ public class Question implements java.io.Serializable
     }
     
      /**
-     * Returns the answer for questions with just one answer
+     * Returns the answer for questions with just one answer.
      */
     public Answer getTheAnswer()
     {
@@ -90,7 +97,7 @@ public class Question implements java.io.Serializable
     }
     
     /**
-     * Returns an answer by index, for questions with more than one possible answer
+     * Returns an answer by index, for questions with more than one possible answer.
      */
     public Answer getIndexedAnswer(int answerNum)
     {
@@ -98,46 +105,44 @@ public class Question implements java.io.Serializable
     }
     
     /**
-     * Returns all of the possible answers
+     * Returns all of the possible answers.
      */
     public ArrayList<Answer> getAllAnswers()
     {
         return answers;
     }
     
-    /**
-     * Returns the question text
-     */
     public String getQuestionText()
     {
         return questionText;
     }
     
      /**
-     * Sets the question text
+     * Sets the question text.
      */
-    public void getQuestionText(String text) // for editing
+    public void setQuestionText(String text) // for editing
     {
         questionText = text;
     }
     
     /**
-     * Returns the mark awarded for the question. Is 0 if wrong or unanswered
+     * Returns the mark awarded for the question only if all correct answers and no others are selected.
      */
     public int getMark()
     {
+        marksAwarded = possibleMarks;
         Iterator<Answer> it = answers.iterator();
         while (it.hasNext()) {
             Answer a = it.next();
-            if(a.getIsSelected() && a.getIsRight()) {
-                marksAwarded = possibleMarks;
+            if((a.getIsSelected() && !a.getIsRight()) || (!a.getIsSelected())) {
+                marksAwarded = 0;
             }
         }
         return marksAwarded;
     }
     
     /**
-     * Returns the mark awarded for the question. Is 0 if wrong or unanswered
+     * Returns the mark awarded for the question. Is 0 if wrong or unanswered.
      */
     public int getPossibleMark()
     {
