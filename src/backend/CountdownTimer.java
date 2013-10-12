5a486c7bc3fa4f1988dc9b2b8ae20aa4f8ea8d21
Chondrokoukis Dimitrios <dimitrios1988@msn.com>
2013-10-12 15:36:20 +0100

    Uploaded latest version of CountdownTimer

---
+++ src/backend/CountdownTimer.java
@@ -0,0 +1,40 @@
+package backend;
+
+import javax.swing.JLabel;
+
+/*
+ * @author Chondrokoukis Dimitrios
+ */
+public class CountdownTimer extends Thread{
+	private int minutes;
+	private volatile boolean isFinished;
+	private volatile JLabel jLabel;
+	
+	public CountdownTimer(int minutes, JLabel jLabel){
+		this.minutes = minutes;
+		this.isFinished = false;
+		this.jLabel = jLabel;
+	}
+	
+	public void run(){
+		while((!isFinished)&&(jLabel.isDisplayable())){
+			jLabel.setText(Integer.toString(minutes));
+			try {
+				sleep(1000*60);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(minutes==0){
+				stops();
+			}
+			minutes--;
+		}
+		stops();
+	}
+	
+	public void stops(){
+		this.isFinished = true;
+	}
+}
+
