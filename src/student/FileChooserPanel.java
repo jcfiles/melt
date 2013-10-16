39df31f27f3e3c8c68d1f3e17becd8a38f7590c3
Justin Chan <lokshunjustin.chan@postgrad.mancheser.ac.uk>
2013-10-16 15:41:49 +0100

    Updated FinishTestPanel. Included a feature that allowed students to save results. Updated FileChooserPanel.

---
+++ src/student/FileChooserPanel.java
@@ -0,0 +1,72 @@
+package student;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import backend.Test_;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+
+public class FileChooserPanel extends JFrame {
+
+	Test_ t;
+	/**
+	 * Create the panel.
+	 */
+	  public FileChooserPanel() throws FileNotFoundException, IOException, ClassNotFoundException {
+		    super("File Chooser Test Frame");
+		    setSize(350, 200);
+		    setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		    Container c = getContentPane();
+		    c.setLayout(new FlowLayout());
+		    
+		    JButton openButton = new JButton("Open");
+		    JButton saveButton = new JButton("Save");
+		    JButton dirButton = new JButton("Pick Dir");
+		    final JLabel statusbar = new JLabel("Output of your selection will go here");
+
+		    // Create a file chooser that opens up as an Open dialog
+		    openButton.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent ae) {
+		        JFileChooser chooser = new JFileChooser();
+		        chooser.setMultiSelectionEnabled(true);
+		        int option = chooser.showOpenDialog(FileChooserPanel.this);
+		        if (option == JFileChooser.APPROVE_OPTION) {
+		          File[] sf = chooser.getSelectedFiles();
+		          String filelist = "nothing";
+		          if (sf.length > 0) filelist = sf[0].getName();
+		          for (int i = 1; i < sf.length; i++) {
+		            filelist += ", " + sf[i].getName();
+		          }
+		          statusbar.setText("You chose " + filelist);
+		        }
+		        else {
+		          statusbar.setText("You canceled.");
+		        }
+		      }
+		    });
+		    
+		    c.add(openButton);
+		    c.add(statusbar);
+	  }
+
+	  public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, IOException {
+		  JFrame frame = new FileChooserPanel();
+		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		  frame.setVisible(true);
+	  }
+
+}
--- src/student/FinishTestPanel.java
+++ src/student/FinishTestPanel.java
@@ -1,14 +1,29 @@
 package student;
 
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.Font;
+
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FinishTestPanel extends JPanel {
 
@@ -58,9 +73,60 @@ public class FinishTestPanel extends JPanel {
 		
 		JLabel lblYourMarkIs = new JLabel("Your mark is");
 		panel.add(lblYourMarkIs);
-		
 		JLabel label = new JLabel(marks);
 		panel.add(label);
+		
+		JButton saveButton = new JButton("Save");
+		final JLabel statusbar = new JLabel("");
+		
+	    // Create a file chooser that opens up as a Save dialog
+
+		// converting parameter "marks" into final, so that it can be used in a nested class.
+		final String finalmarks = marks;
+		
+	    saveButton.addActionListener(new ActionListener() {
+	      public void actionPerformed(ActionEvent ae) {
+	        JFileChooser chooser = new JFileChooser();
+	        int option = chooser.showSaveDialog(FinishTestPanel.this);
+	        if (option == JFileChooser.APPROVE_OPTION) {
+	        	statusbar.setText("You saved " + ((chooser.getSelectedFile()!=null)?
+	        			chooser.getSelectedFile().getName():"nothing"));
+	          String fileName = chooser.getSelectedFile().getAbsolutePath();
+	          
+				try {
+					
+		            // create a new file with an ObjectOutputStream
+					BufferedWriter out = new BufferedWriter(new FileWriter(fileName));			
+					
+					// record the date and time in the file.
+					DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+					Date date = new Date();
+					out.write(dateFormat.format(date));
+
+					// write the marks in the file.
+		            out.write("\n" + finalmarks);
+
+		            // close the stream
+		            out.close();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+
+	        }
+	        else {
+	          statusbar.setText("You canceled.");
+	        }
+	      }
+	    });
+		
+
+		
+		panel.add(saveButton);
+		panel.add(statusbar);
+		
+		// Back Button 
 		GridBagConstraints gbc_backButton = new GridBagConstraints();
 		gbc_backButton.gridx = 0;
 		gbc_backButton.gridy = 6;
