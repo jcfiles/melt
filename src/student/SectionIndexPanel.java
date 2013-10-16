c99fbfa08bb32b7ff66ae6b5e3923444c0fcfff5
Chondrokoukis Dimitrios <dimitrios1988@msn.com>
2013-10-16 19:29:32 +0100

    Added an edit button in SectionIndexPanel that is appeared only to the teacher

--- src/student/SectionIndexPanel.java
+++ src/student/SectionIndexPanel.java
@@ -11,42 +11,95 @@ import backend.Section;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
+import java.awt.Insets;
 
 public class SectionIndexPanel extends JPanel {
 	
     private StudentTestController controller;
     private ArrayList<Section> sections;
+    private JPanel panelSections;
 
     /**
      * Create the panel.
      */
     public SectionIndexPanel(final StudentTestController controller) {
         this.controller = controller;
-        
         controller.setSectionIndexPanel(this); // Passes a reference to section index panel to the controller
         
 	GridBagLayout gridBagLayout = new GridBagLayout();
 	gridBagLayout.columnWidths = new int[]{0, 0};
-	gridBagLayout.rowHeights = new int[]{0, 0};
+	gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
 	gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-	gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+	gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
 	setLayout(gridBagLayout);
-        sections = controller.getTest().getAllSections();
-        int i=0;
-	for(Section section: sections){
-            buildSectionChooserPanel(i++);
-        }
-        
-        JButton buttonSubmitTest = new JButton("Submit Test");
-	buttonSubmitTest.addMouseListener(new MouseAdapter() {
+	panelSections = new JPanel();
+    sections = controller.getTest().getAllSections();
+	
+    JPanel panelEdit = new JPanel();
+	GridBagConstraints gbc_panelEdit = new GridBagConstraints();
+	gbc_panelEdit.fill = GridBagConstraints.BOTH;
+	gbc_panelEdit.insets = new Insets(0, 0, 5, 0);
+	gbc_panelEdit.gridx = 0;
+	gbc_panelEdit.gridy = getComponentCount();
+	add(panelEdit, gbc_panelEdit);
+	GridBagLayout gbl_panelEdit = new GridBagLayout();
+	gbl_panelEdit.columnWidths = new int[]{0, 0};
+	gbl_panelEdit.rowHeights = new int[]{0, 0};
+	gbl_panelEdit.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+	gbl_panelEdit.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+	panelEdit.setLayout(gbl_panelEdit);
+	
+	JButton buttonEdit = new JButton("Edit");
+	GridBagConstraints gbc_buttonEdit = new GridBagConstraints();
+	gbc_buttonEdit.anchor = GridBagConstraints.EAST;
+	gbc_buttonEdit.gridx = 0;
+	gbc_buttonEdit.gridy = 0;
+	panelEdit.add(buttonEdit, gbc_buttonEdit);
+	
+	if(!MainGui.isSetter){
+		panelEdit.setVisible(false);
+	}
+    
+	GridBagConstraints gbc_panelSections = new GridBagConstraints();
+	gbc_panelSections.insets = new Insets(0, 0, 5, 0);
+	gbc_panelSections.fill = GridBagConstraints.BOTH;
+	gbc_panelSections.gridx = 0;
+	gbc_panelSections.gridy = getComponentCount();
+	add(panelSections, gbc_panelSections);
+	GridBagLayout gbl_panelSections = new GridBagLayout();
+	gbl_panelSections.columnWidths = new int[]{0, 0};
+	gbl_panelSections.rowHeights = new int[]{0, 0, 0};
+	gbl_panelSections.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+	gbl_panelSections.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+	panelSections.setLayout(gbl_panelSections);
+	
+	JPanel panelBack = new JPanel();
+	GridBagConstraints gbc_panelBack = new GridBagConstraints();
+	gbc_panelBack.fill = GridBagConstraints.BOTH;
+	gbc_panelBack.gridx = 0;
+	gbc_panelBack.gridy = getComponentCount();
+	add(panelBack, gbc_panelBack);
+	GridBagLayout gbl_panelBack = new GridBagLayout();
+	gbl_panelBack.columnWidths = new int[]{75, 0};
+	gbl_panelBack.rowHeights = new int[]{29, 0};
+	gbl_panelBack.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+	gbl_panelBack.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+	panelBack.setLayout(gbl_panelBack);
+	
+	JButton buttonBack = new JButton("Back");
+	GridBagConstraints gbc_buttonBack = new GridBagConstraints();
+	gbc_buttonBack.gridx = 0;
+	gbc_buttonBack.gridy = 0;
+	panelBack.add(buttonBack, gbc_buttonBack);
+	buttonBack.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {controller.endTest();}
 	});
-	GridBagConstraints gbc_buttonSubmitTest = new GridBagConstraints();
-	//gbc_buttonSubmitTest.anchor = GridBagConstraints.NORTH;
-	gbc_buttonSubmitTest.gridx = 0;
-	gbc_buttonSubmitTest.gridy = 1000;
-	this.add(buttonSubmitTest, gbc_buttonSubmitTest); 
+	
+	int i=0;
+	for(Section section: sections){
+            buildSectionChooserPanel(i++);
+        }
     }
     
     private void buildSectionChooserPanel(int index) {
@@ -55,9 +108,10 @@ public class SectionIndexPanel extends JPanel {
         	//panel.setbuttonSelectEnabled(false);
         }
         GridBagConstraints gbc_panel = new GridBagConstraints();
-        gbc_panel.fill = GridBagConstraints.BOTH;
+        gbc_panel.anchor = GridBagConstraints.NORTH;
+        gbc_panel.fill = GridBagConstraints.HORIZONTAL;
         gbc_panel.gridx = 0;
         gbc_panel.gridy = index;
-        add(panel, gbc_panel);
+        panelSections.add(panel, gbc_panel);
     }
 }
