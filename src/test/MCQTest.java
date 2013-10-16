57c40a9c34e55a01bf5e49b48612e447ada8dcf2
Stefan <sml@cs.man.ac.uk>
2013-10-16 22:42:35 +0100

    added some more backend test cases

---
+++ src/test/MCQTest.java
@@ -0,0 +1,64 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MCQTest {
+
+	@Test
+	public void testGetMarksAwarded() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllAnswers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetQuestionText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetQuestionText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPossibleMarks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPossibleMarks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAnswer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSwapAnswers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAnswer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIndexedAnswer() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/SectionTest.java
@@ -0,0 +1,89 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SectionTest {
+
+	@Test
+	public void testEditSection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSectionTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSectionIntroText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSectionTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPossibleSectionMarks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSwapQuestions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetQuestionsList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEndSection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGradeSection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLockSection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsLocked() {
+		fail("Not yet implemented");
+	}
+
+}
