c03efd71b0eaad2aaf63b3c4e29647276ddfc8f4
Stefan <sml@cs.man,.>
2013-10-17 02:27:36 +0100

    More tests in the Test package

--- src/test/MCQTest.java
+++ src/test/MCQTest.java
@@ -2,63 +2,106 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+import backend.*;
 
 public class MCQTest {
 
 	@Test
 	public void testGetMarksAwarded() {
-		fail("Not yet implemented");
+		MCQ mcq = new MCQ("This is a test question");
+		int expResult = 0;
+		assertEquals(expResult, mcq.getMarksAwarded());
 	}
 
 	@Test
 	public void testGetAllAnswers() {
-		fail("Not yet implemented");
+		ArrayList<Answer> testAnswer = new ArrayList<Answer>();
+		MCQ mcq = new MCQ("This is a test question", testAnswer);
+		ArrayList expResult = testAnswer;
+		assertEquals(expResult, mcq.getAllAnswers());
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		MCQ mcq = new MCQ("This is a test question");
+		String expResult = "This is a test question\n\n";
+		assertEquals(expResult, mcq.toString());
 	}
 
 	@Test
 	public void testGetQuestionText() {
-		fail("Not yet implemented");
+		MCQ mcq = new MCQ("This is a test question");
+		String expResult = "This is a test question";
+		assertEquals(expResult, mcq.getQuestionText());
 	}
 
 	@Test
 	public void testSetQuestionText() {
-		fail("Not yet implemented");
+		MCQ mcq = new MCQ("This is a test question");
+		mcq.setQuestionText("This is a different test question");
+		String expResult = "This is a different test question";
+		assertEquals(expResult, mcq.getQuestionText());
 	}
 
 	@Test
 	public void testGetPossibleMarks() {
-		fail("Not yet implemented");
+		MCQ mcq = new MCQ("This is a test question", 2);
+		int expResult = 2;
+		assertEquals(expResult, mcq.getPossibleMarks());
 	}
 
 	@Test
 	public void testSetPossibleMarks() {
-		fail("Not yet implemented");
+		MCQ mcq = new MCQ("This is a test question");
+		mcq.setPossibleMarks(2);
+		int expResult = 2;
+		assertEquals(expResult, mcq.getPossibleMarks());
 	}
 
 	@Test
 	public void testAddAnswer() {
-		fail("Not yet implemented");
+		MCQ mcq = new MCQ("This is a test question");
+		Answer testAnswer = new Answer("testAnswer", true);
+		mcq.addAnswer(testAnswer);
+		ArrayList<Answer> expResult = new ArrayList<Answer>();
+		expResult.add(testAnswer);
+		assertEquals(expResult, mcq.getAllAnswers());
 	}
 
 	@Test
 	public void testSwapAnswers() {
-		fail("Not yet implemented");
+		Answer testAnswer = new Answer("testAnswer", true);
+		Answer testAnswer2 = new Answer("testAnswer2", false);
+		ArrayList testAnswers = new ArrayList<Answer>();
+		testAnswers.add(testAnswer);
+		testAnswers.add(testAnswer2);
+		MCQ mcq = new MCQ("This is a test question", testAnswers);
+		mcq.swapAnswers(mcq.getIndexedAnswer(0), mcq.getIndexedAnswer(1));
+		ArrayList expResult = new ArrayList<Answer>();
+		expResult.add(testAnswer2);
+		expResult.add(testAnswer);
+		assertEquals(expResult, mcq.getAllAnswers());
 	}
 
 	@Test
 	public void testSetAnswer() {
-		fail("Not yet implemented");
+		Answer testAnswer = new Answer("testAnswer", true);
+		MCQ mcq = new MCQ("This is a test question", testAnswer);
+		mcq.setAnswer("a different answer", false, 0);
+		Answer expResult = new Answer("a different answer", false);
+		assertEquals(expResult, mcq.getIndexedAnswer(0));
+		//TODO there is a bug in the implementation of this
 	}
 
 	@Test
 	public void testGetIndexedAnswer() {
-		fail("Not yet implemented");
+		Answer testAnswer = new Answer("testAnswer", true);
+		MCQ mcq = new MCQ("This is a test question", testAnswer);
+		Answer expResult = testAnswer;
+		assertEquals(expResult, mcq.getIndexedAnswer(0));
 	}
 
 }
--- src/test/SectionTest.java
+++ src/test/SectionTest.java
@@ -2,88 +2,100 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+import backend.*;
 
 public class SectionTest {
 
 	@Test
 	public void testEditSection() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		testSection.editSection("newTitle", "newIntro", 2);
+		Section expResult = new Section("newTitle", "newIntro", 2);
+		assertEquals(expResult.getSectionTitle(), testSection.getSectionTitle());
+		assertEquals(expResult.getSectionIntroText(), testSection.getSectionIntroText());
+		assertEquals(expResult.getSectionTime(), testSection.getSectionTime());
 	}
 
 	@Test
 	public void testGetSectionTitle() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		String expResult = "testTitle";
+		assertEquals(expResult, testSection.getSectionTitle());
 	}
 
 	@Test
 	public void testGetSectionIntroText() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		String expResult = "testIntro";
+		assertEquals(expResult, testSection.getSectionIntroText());
 	}
 
 	@Test
 	public void testGetSectionTime() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		int expResult = 1;
+		assertEquals(expResult, testSection.getSectionTime());
 	}
 
 	@Test
 	public void testGetPossibleSectionMarks() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		int expResult = 0;
+		assertEquals(expResult, testSection.getPossibleSectionMarks());
 	}
 
 	@Test
-	public void testAddQuestion() {
-		fail("Not yet implemented");
+	public void testAddQuestion() throws InvalidFTBQFormatException {
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		FIBQ fibq = new FIBQ("This is a [test] question.");
+		testSection.addQuestion(fibq);
+		ArrayList<Question> expResult = new ArrayList<Question>();
+		expResult.add(fibq);
+		assertEquals(expResult, testSection.getQuestionsList());
 	}
 
 	@Test
-	public void testRemoveQuestion() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testInsertQuestion() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSwapQuestions() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetQuestion() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetQuestionsList() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEndSection() {
-		fail("Not yet implemented");
+	public void testRemoveQuestion() throws InvalidFTBQFormatException {
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		FIBQ fibq = new FIBQ("This is a [test] question.");
+		testSection.addQuestion(fibq);
+		ArrayList<Question> expResult = new ArrayList<Question>();
+		testSection.removeQuestion(fibq);
+		assertEquals(expResult, testSection.getQuestionsList());
 	}
 
 	@Test
 	public void testGradeSection() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		int expResult = 0;
+		assertEquals(expResult, testSection.gradeSection());
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		String expResult = "testTitle\ntestIntro\n\n";
+		assertEquals(expResult, testSection.toString());
 	}
 
 	@Test
 	public void testLockSection() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		boolean expResult = false;
+		assertEquals(expResult, testSection.isLocked());
+		testSection.lockSection();
+		expResult = true;
+		assertEquals(expResult, testSection.isLocked());
 	}
 
 	@Test
 	public void testIsLocked() {
-		fail("Not yet implemented");
+		Section testSection = new Section("testTitle", "testIntro", 1);
+		boolean expResult = false;
+		assertEquals(expResult, testSection.isLocked());
 	}
 
 }
