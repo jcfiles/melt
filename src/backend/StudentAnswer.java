8ef0abdd710b6886491f46d5f09276743717942b
Stefan <sml@cs.man.ac.uk>
2013-10-23 02:15:48 +0100

    Added first version of EssayQ.java, SlotQ.java (with exception class) and StudentAnswer.java. There will be bugs. Feel free to change things :P

---
+++ src/backend/StudentAnswer.java
@@ -0,0 +1,73 @@
+package backend;
+
+/*
+ * A class to record a student's answer which is used for manual marking.
+ * others than Answer.java which is used to store a possible predefined answer.
+ */
+
+public class StudentAnswer {
+	
+	protected Question question; //to get the question to this answer
+
+	protected String answer;
+	protected int marksAwarded;
+	protected String feedback;
+	protected boolean isMarked;
+
+	//empty constructor
+	public StudentAnswer()
+	{
+		this.answer = null;
+		this.marksAwarded = 0;
+		this.feedback = null;
+		this.isMarked = false;
+	}
+	
+	//constructor with student answer
+	public StudentAnswer(String answer)
+	{
+		this.answer = answer;
+		this.marksAwarded = 0;
+		this.feedback = null;
+		this.isMarked = false;
+	}
+	
+	
+	//Setters and Getters
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public int getMarksAwarded() {
+		return marksAwarded;
+	}
+
+	public void setMarksAwarded(int marksAwarded) {
+		this.marksAwarded = marksAwarded;
+	}
+	
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	
+	public Question getQuestion() {
+		return question;
+	}
+	
+	public boolean isMarked() {
+		return isMarked;
+	}
+
+	public void setMarked(boolean isMarked) {
+		this.isMarked = isMarked;
+	}
+
+}
